36b36f50bba32aa2e47893f443f418bcccbeab39
ddeeddii <dedialts@gmail.com>
2021-03-12 12:51:28 +0100

    init

---
+++ README.md
@@ -0,0 +1,9 @@
+Simple python script to match words from a file and print them out.
+
+## Usage
+Replace "words.txt" with whatever text file you want.
+ 
+Also replace "STRING" with whatever you want to match 
+
+## Credits
+Word list: [powerlanguage/word-lists](https://github.com/powerlanguage/word-lists)
---
+++ words.py
@@ -0,0 +1,15 @@
+x = 0
+listgood = []
+
+with open("words.txt") as file:
+  lines = [i.strip() for i in file]
+
+for i in lines:
+    if "dis" in lines[x]:
+        listgood.append(lines[x])
+    x += 1
+
+# make array into string, optional
+listgoodstr = ' '.join([str(elem) for elem in listgood]) 
+
+print(listgoodstr)
